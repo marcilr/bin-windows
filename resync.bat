f99db1b0ba3c78fa6c641e46e95bbdb61c5f73ac
Marcil <remarcil-dev@dot.soa.alaska.gov>
2020-08-05 15:54:19 -0800

    resync.bat   Script read manifest file in current working directory   and checkout or update any of the git or subversion   repositories listed.

---
+++ resync.bat
@@ -0,0 +1,8 @@
+REM resync.bat
+REM Created Wed 05 Aug 2020 03:52:44 PM AKDT
+REM by Raymond E. Marcil <remarcil@alaska.gov>
+REM
+REM Script read manifest file in current working directory
+REM and checkout or update any of the git or subversion 
+REM repositories listed.
+REM
